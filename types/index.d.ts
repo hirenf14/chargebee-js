c0502b744c5baecf5d734a5d27317054e1a84924
hirenf14 <hirenf14@gmail.com>
2024-01-29 22:33:49 +0530

    feat: extended type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,13 +1,54 @@
-export type Chargebee = any;
+export type Chargebee = {
+  init: (config: ChargebeeConfig) => ChargebeeInstance;
+  getInstance: () => ChargebeeInstance;
+};
 
+export type ChargebeeInstance = any;
 
+/**
+ * Chargebee init config
+ *
+ * @docs https://www.chargebee.com/checkout-portal-docs/cb-library-api-ref.html#init
+ */
 export type ChargebeeConfig = {
-    site: string;
-    domain?: string;
-    businessEntityId?: string;
-}
+  /**
+   * Your site name.
+   * `site` is required.
+   */
+  site: string;
+  /**
+   * Your custom domain.
+   * Eg: https://billing.yourdomain.com
+   */
+  domain?: string;
+  
+  /**
+   * The unique ID of the business entity under whose context the cart object or the drop-in script should work.
+   * If not provided, the default business entity defined for the site is considered.
+   * Note: This parameter is applicable only when multiple business entities have been created for the site.
+   * Multiple Business Entities is currently in early access. Contact eap@chargebee.com to join the Early Adopter Program and enable this feature.
+   */
+  businessEntityId?: string;
+  /**
+   * This will be used for Chargebee.js functions (VAT validation, estimate APIs) and components & fields tokenization.
+   * See more info on, how to use a publishable key.
+   * 
+   * @docs https://www.chargebee.com/docs/2.0/api_keys.html#types-of-api-keys_publishable-key
+   */
+  publishableKey?: boolean;
+  isItemsModel?: boolean;
+  enableRedirectMode?: boolean;
+  iframeOnly?: boolean;
+  enableGATracking?: boolean;
+  enableGTMTracking?: boolean;
+  enableFBQTracking?: boolean;
+  enableFriendbuyTracking?: boolean;
+  enableRefersionTracking?: boolean;
+};
 
-export const loadChargebee: (config: ChargebeeConfig) => Promise<Chargebee | undefined>;
+export type LoadChargebee = (
+  config: ChargebeeConfig
+) => Promise<ChargebeeInstance | null>;
 
 declare global {
   interface Window {
