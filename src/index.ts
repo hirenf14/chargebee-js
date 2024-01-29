b2dffbdfbfa5b1546a59549f12aa1664df505392
hirenf14 <hirenf14@gmail.com>
2024-01-29 22:35:18 +0530

    feat: added load/init methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
-const chargeBee = undefined;
+import {
+  Chargebee,
+  ChargebeeConfig,
+  ChargebeeInstance,
+  LoadChargebee,
+} from "../types";
+import { loadScript } from "./shared";
 
-export default chargeBee;
+let cbPromise: Promise<Chargebee | null> | null;
+let loadCalled = false;
+
+const getCbPromise: () => Promise<Chargebee | null> = () => {
+  if (cbPromise) {
+    return cbPromise;
+  }
+
+  cbPromise = loadScript().catch((error) => {
+    // clear cache on error
+    cbPromise = null;
+    return Promise.reject(error);
+  });
+  return cbPromise;
+};
+ 
+// Execute our own script injection after a tick to give users time to do their
+// own script injection.
+Promise.resolve()
+  .then(() => getCbPromise())
+  .catch((error) => {
+    if (!loadCalled) {
+      console.warn(error);
+    }
+  });
+
+export const loadChargebee: LoadChargebee = (config) => {
+  loadCalled = true;
+  config.site
+  // if previous attempts are unsuccessful, will re-load script
+  return getCbPromise().then((maybeChargebee) =>
+    initChargebee(maybeChargebee, config)
+  );
+};
+
+export const initChargebee = (
+  maybeChargebee: Chargebee | null,
+  config: ChargebeeConfig
+): ChargebeeInstance | null => {
+  if (maybeChargebee === null) {
+    return null;
+  }
+
+  const cbInstance = maybeChargebee.init(config);
+  return cbInstance;
+};
