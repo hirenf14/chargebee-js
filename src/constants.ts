9f147bd869f7e5cb30eea5cce8f6682d6b499ed6
hirenf14 <hirenf14@gmail.com>
2024-01-29 22:34:04 +0530

    feat: added constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SCRIPT_URL = "https://js.chargebee.com/v2/chargebee.js";
+export const SCRIPT_URL_REGEX =
+  /^https:\/\/js\.chargebee\.com\/v2\/chargebee\.js\/?(\?.*)?$/;
+
+export const EXISTING_SCRIPT_MESSAGE =
+  "loadChargebee was called but a script already exists in the document;";
