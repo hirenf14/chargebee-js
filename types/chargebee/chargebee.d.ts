377519243a8caa02eaf5676908636c401aec6b3a
hirenf14 <hirenf14@gmail.com>
2024-01-30 15:26:54 +0530

    docs: added type doc for chargebee

--- types/chargebee/chargebee.d.ts
+++ types/chargebee/chargebee.d.ts
@@ -2,6 +2,17 @@ import { ChargebeeConfig } from "./config";
 import { ChargebeeInstance } from "./instance";
 
 export type Chargebee = {
+    /**
+     * Initiate Chargebee Instance
+     * 
+     * @param config ChargebeeConfig
+     * @returns ChargebeeInstance
+     */
   init: (config: ChargebeeConfig) => ChargebeeInstance;
+  /**
+   * Get initiated Chargebee Instance.
+   * 
+   * @returns ChargebeeInstance
+   */
   getInstance: () => ChargebeeInstance;
 };
